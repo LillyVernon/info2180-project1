72632791822b43c7f69c6cb68ad9a3135cb569ac
Lilly Vernon <littletomoya@yahoo.com>
2020-10-20 23:27:42 -0500

    Images added along with task 1 and 2

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    let firsth2 = document.getElementsByClassName("overview-hero-text")[0].id = "h21";
+
+    document.getElementsByTagName("img")[0].height = 350;
+    document.getElementsByTagName("img")[0].width = 300;
+    document.getElementsByTagName("img")[1].height = 80;
+    document.getElementsByTagName("img")[1].width = 100;
+    document.getElementsByTagName("img")[2].height = 80;
+    document.getElementsByTagName("img")[2].width = 100;
+    document.getElementsByTagName("img")[3].height = 80;
+    document.getElementsByTagName("img")[3].width = 100;
+    /*document.getElementsByClassName("learn-about")[11].height = "5";
+    document.getElementsByClassName("learn-about")[11].width = "5";
+       document.getElementsByClassName("overview-hero-image")[0].style.width = "10";
+    document.getElementsByClassName("overview-hero-image")[0].style.height = "100";
+    
+    */
+
+
+});
