71581da285b05b57eefda7ecedf60204a60e09ee
Lilly Vernon <littletomoya@yahoo.com>
2020-10-21 00:22:30 -0500

    Contents of the newsletter centered

--- scripts.js
+++ scripts.js
@@ -10,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementsByTagName("img")[2].width = 100;
     document.getElementsByTagName("img")[3].height = 80;
     document.getElementsByTagName("img")[3].width = 100;
+    document.getElementsByTagName("img")[4].height = 200;
+    document.getElementsByTagName("img")[5].width = 250;
+    document.getElementsByTagName("img")[5].style.cssFloat = "right";
     /*document.getElementsByClassName("learn-about")[11].height = "5";
     document.getElementsByClassName("learn-about")[11].width = "5";
        document.getElementsByClassName("overview-hero-image")[0].style.width = "10";
